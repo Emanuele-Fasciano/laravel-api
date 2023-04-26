88d5219429cd0dc554e2bd01ffc6f09bf1f0e181
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-04-26 21:49:08 +0200

    add api show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -53,7 +53,21 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        //
+        // query per recuperare il project da stampare tramite l'id
+        $project = Project::where('id', $id)
+            ->with('type', 'technologies')
+            ->first();
+
+        if ($project->image)
+            $project->image = url('storage/' . $project->image);
+
+        else
+            $project->image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png';
+
+        // se l'id del post non esiste ritorna un'errore 404
+        if (!$project) return response(null, 404);
+
+        return response()->json($project);
     }
 
     /**
